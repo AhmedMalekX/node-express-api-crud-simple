d1d6aab34d1f1ea4305823731962e51602a6ceac
Ahmed Malek <ahmedmalekvic@mail.ru>
2020-08-12 23:27:15 +0200

    Create controllers folder and separate functions from routes

---
+++ controllers/users.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+let users = [];
+
+// Get All Users
+
+export const getAllUsers = (req, res) => {
+  res.json(users);
+};
+
+// Create New User
+
+export const createNewUser = (req, res) => {
+  const user = req.body;
+
+  const userId = uuidv4();
+
+  const userWithID = { ...user, id: userId };
+
+  users.push(userWithID);
+  res.send(`User added to database 👍`);
+};
+
+// Get Sigle User
+
+export const getSigleUser = (req, res) => {
+  const { id } = req.params;
+
+  const signleUser = users.find((u) => u.id === id);
+
+  res.send(signleUser);
+};
+
+// Update Signle User
+
+export const updateSignleUser = (req, res) => {
+  const { id } = req.params;
+
+  const user = users.find((user) => user.id === id);
+
+  const { firstName, lastName, location, age } = req.body;
+
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
+  if (location) user.location = location;
+
+  res.send(`User with ID: ${id} Updated 🐱‍🏍`);
+};
+
+//  Delete User
+
+export const deleteSingleUser = (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with ID: ${id} deleted 👏`);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,30 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+
+import {
+  getAllUsers,
+  createNewUser,
+  deleteSingleUser,
+  getSigleUser,
+  updateSignleUser,
+} from '../controllers/users.js';
+
+// import userControllers from '../controllers/users';
 
 const router = express.Router();
 
-const users = [];
-
 // Get All Users
-router.get('/', (req, res) => {
-  res.json(users);
-});
+router.get('/', getAllUsers);
 
 // Create New User
-router.post('/', (req, res) => {
-  const user = req.body;
-
-  const userId = uuidv4();
-
-  const userWithID = { ...user, id: userId };
-
-  users.push(userWithID);
-  res.send(`User added to database 👍`);
-});
+router.post('/', createNewUser);
 
 // Get Sigle User
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
+router.get('/:id', getSigleUser);
 
-  const signleUser = users.find((u) => u.id === id);
-
-  res.send(signleUser);
-});
-
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-
-  const user = users.find((user) => user.id === id);
-
-  const { firstName, lastName, location, age } = req.body;
-
-  if (firstName) user.firstName = firstName;
-  if (lastName) user.lastName = lastName;
-  if (age) user.age = age;
-  if (location) user.location = location;
-
-  res.send(`User with ID: ${id} Updated 🐱‍🏍`);
-});
+// Update Signle User
+router.patch('/:id', updateSignleUser);
 
 //  Delete User
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-
-  const deletedUser = users.filter((user) => user.id !== id);
-
-  res.send(`User with ID: ${id} deleted 👏`);
-});
+router.delete('/:id', deleteSingleUser);
 
 export default router;
